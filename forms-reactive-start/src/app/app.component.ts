1d06bbc18200ae689644fc627ee70b2c3a5fa0bd
Welly Setiawan Limantoro <welly.eic06g@nctu.edu.tw>
2021-02-01 16:30:45 +0800

    added syntax for checking state of value & status change

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -21,6 +21,15 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male', Validators.required),
       'hobbies': new FormArray([])
     });
+
+    /* Syntax for checking state of value & status change */
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    );
   }
 
   onSubmit() {
