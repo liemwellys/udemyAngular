ba53418e76c31f4a52adddfad162abe594414083
liemwellys <welly.eic06g@nctu.edu.tw>
2021-02-19 19:26:08 +0800

    added a recipe in an array

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This simply a test', 
+      `https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=
+      rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=
+      format&fit=crop&w=714&q=80`)
+  ];
 
   constructor() { }
 
