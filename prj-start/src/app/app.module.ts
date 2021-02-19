4cfdef762286cece844175bd9816eadb2c483686
liemwellys <welly.eic06g@nctu.edu.tw>
2021-02-19 16:34:52 +0800

    added shoppping-edit component under shopping-list

--- prj-start/src/app/app.module.ts
+++ prj-start/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShopppingEditComponent } from './shopping-list/shoppping-edit/shoppping-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    ShopppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ prj-start/src/app/shopping-list/shoppping-edit/shoppping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppping-edit',
+  templateUrl: './shoppping-edit.component.html',
+  styleUrls: ['./shoppping-edit.component.css']
+})
+export class ShopppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
