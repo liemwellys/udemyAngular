86d89f9477bbf6a6bafc3ca12beb31427ba63c58
liemwellys <welly.eic06g@nctu.edu.tw>
2021-07-20 15:04:27 +0800

    replace ShopppingEditComponent typo with ShoppingEditComponent

--- prj-start/src/app/app.module.ts
+++ prj-start/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShopppingEditComponent } from './shopping-list/shoppping-edit/shoppping-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import { ShopppingEditComponent } from './shopping-list/shoppping-edit/shoppping
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingListComponent,
-    ShopppingEditComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
