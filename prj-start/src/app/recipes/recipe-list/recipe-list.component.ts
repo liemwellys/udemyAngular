01da1509b62b060c75b03126eaeb12eb686eebba
liemwellys <welly.eic06g@nctu.edu.tw>
2021-07-20 15:57:53 +0800

    added dummy data to display the recipe

--- prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-start/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,10 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This simply a test', 
       `https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=
       rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=
+      format&fit=crop&w=714&q=80`),
+    new Recipe('A Test Recipe', 'This simply a test', 
+      `https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=
+      rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=
       format&fit=crop&w=714&q=80`)
   ];
 
